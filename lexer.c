89b89c1dde64f41fedb70c4648b55cfdda92bc63
polaris <131747396+Dogude@users.noreply.github.com>
2026-09-02 20:19:04 +0200

    Refactor lexer.c to improve structure and readability

--- lexer.c
+++ lexer.c
@@ -1,58 +1,20 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "lexer.h"
 
-#pragma comment(linker, "/STACK:8388608")
+extern void parser();
 
-#define CHUNK_SIZE 4096 
-#define FILE_NAME_LEN 256
-#define LEXEME_SIZE 1024
+struct Token Token;
+struct FileReader FileReader;
 
-#define MAX_DEPTH (1024*3)
-int recursion_depth;
-
-typedef struct {
-	unsigned char* data; 
-	size_t size;         
-	size_t capacity;   
-} x64_Opcodes;
-
-enum Type {
-	Identifier,
-	StringLiteral,
-	Ull_Literal,
-	Integer_Literal,
-	Long,
-	Int,
-	Unsigned,
-	Struct,
-	Enum,
-	Union,
-	Plus,
-	PlusEqual,
-	SemiColomn
-};
-
-struct FileReader {
-	FILE* file;
-	unsigned char buffer[CHUNK_SIZE];
-	size_t line;
-	size_t last_line;
-	size_t pos;         
-	size_t size;         
-	int eof;
-} FileReader;
-	
 typedef struct IncludeStack {
-	FILE *file;
-	char *filename;
+	FILE* file;
+	char* filename;
 	size_t pos;
 	size_t line;
 	size_t last_line;
 	struct IncludeStack* prev;
 } IncludeStack;
 
-void push_file(IncludeStack** top, char* file_name, struct FileReader* fr) {
+void push_file(IncludeStack** top, char* file_name) {
 			
 	char* fname = malloc(strlen(file_name) + 1);
 	if (!fname) {
@@ -77,23 +39,23 @@ void push_file(IncludeStack** top, char* file_name, struct FileReader* fr) {
 	}
 	
 	node->file = file;
-	fr->file = node->file;
+	FileReader.file = node->file;
 	node->filename = fname;
 	node->prev = NULL;
 	
 	if (*top == NULL) {		
 		*top = node;
 	} else {
-		(*top)->pos = fr->pos;
-		(*top)->line = fr->line;
-		(*top)->last_line = fr->last_line;
+		(*top)->pos = FileReader.pos;
+		(*top)->line = FileReader.line;
+		(*top)->last_line = FileReader.last_line;
 		node->prev = *top;
 		*top = node;
 	}
 	
-	fr->pos = 0;
-	fr->line = 1;
-	fr->last_line = 0;
+	FileReader.pos = 0;
+	FileReader.line = 1;
+	FileReader.last_line = 0;
 
 }
 
@@ -102,99 +64,34 @@ void pop_file(IncludeStack** top) {
 
 }
 
-struct Token {
-	enum Type type;
-	size_t line;
-	char *lexeme;	
-	union {
-		char c_literal;
-		int integral;
-		unsigned int u_literal;
-		long long long_integral;
-		unsigned long long u_long_literal;
-		float single_precison;
-		double double_precision;
-	} numbers;	
-} Token;
-
-typedef enum {
-	SYM_VARIABLE,
-	SYM_TYPEDEF,
-	SYM_FUNCTION,
-	SYM_STRUCT,
-	SYM_ENUM,
-	SYM_LABEL	
-} SymbolKind;
-
-struct Symbol {
-    char* name;              // Ortak alan (herkes için)
-	SymbolKind kind;         // Hangi tip olduğunu belirten TAG
-    int type;                // C tipi (TYPE_INT, TYPE_FLOAT vs.)
-    int offset;              // Stack/global offset'i
-	
-    union {                  // Her tipe özel alanlar
-        struct {
-            int array_size;  // Dizi boyutu
-        } variable;
-
-        struct {
-            int original_type;   // typedef int my_int; -> original_type = TYPE_INT
-            int pointer_depth;   // typedef int* my_ptr; -> pointer_depth = 1
-        } typedef_info;
-
-        struct {
-            int param_count;
-            int return_type;
-            struct Symbol** params;  // Parametre sembolleri
-        } function;
-
-        struct {
-            int size;             // Struct'ın toplam boyutu
-            int field_count;
-            struct Symbol** fields;  // Alanlar
-        } struct_info;
-
-        struct {
-            int enum_value;       // enum sabitinin değeri
-        } enum_info;
-    } fields;
-
-} ParserState ;
-
-struct Scope {
-	struct Symbol** table; 
-	struct Scope* parent; 
-};
-
-struct Scope* current_scope;
-
-int next_char(struct FileReader* fr) {
-	if (fr->pos >= fr->size) {
-		fr->size = fread(fr->buffer, 1, CHUNK_SIZE, fr->file);
-		fr->pos = 0;
-		if (fr->size == 0) {
-			fr->eof = 1;
+int next_char() {
+	if (FileReader.pos >= FileReader.size) {
+		FileReader.size = fread(FileReader.buffer, 1, CHUNK_SIZE, FileReader.file);
+		FileReader.pos = 0;
+		if (FileReader.size == 0) {
+			FileReader.eof = 1;
 			return EOF;
 		}
 	}
-	return fr->buffer[fr->pos++];
+	return FileReader.buffer[FileReader.pos++];
 }
 
-int next_peek(struct FileReader* fr) {
+int peek() {
 	
-	if (fr->pos < fr->size ) {
-		return fr->buffer[fr->pos];		
+	if (FileReader.pos < FileReader.size ) {
+		return FileReader.buffer[FileReader.pos];
 	}
 	else {
-		fr->size = fread(fr->buffer, 1, CHUNK_SIZE, fr->file);
-		fr->pos = 0;
-		if (fr->size == 0) {
-			fr->eof = 1;
+		FileReader.size = fread(FileReader.buffer, 1, CHUNK_SIZE, FileReader.file);
+		FileReader.pos = 0;
+		if (FileReader.size == 0) {
+			FileReader.eof = 1;
 			return EOF;
 		}
-		return fr->buffer[fr->pos];
+		return FileReader.buffer[FileReader.pos];
 	}				
 }
+	
 
 int is_digit(int c) {
 	return c >= '0' && c <= '9';
@@ -206,117 +103,126 @@ int is_alpha(int c) {
 		c == '_';
 }
 
+int is_xdigit(int c) {
+	return (c >= 'a' && c <= 'f') ||
+		(c >= 'A' && c <= 'F') ||
+		(c >= '0' && c <= '9');
+}
+
 void print_line() {
 	
-
-}
-
-void number(int c) {
-	
-	int next = next_peek(&FileReader);
-
-	if (c == '0' && (next == 'b' || next == 'B')) {
+	size_t line = FileReader.last_line;
+	size_t index = FileReader.pos - FileReader.last_line;
 		
-
-	}
-	else if (c == '0' && (next == 'x' || next == 'X')) {
-		
-
-
-	}
-	else if (c == '0' && is_digit(next)) {
-		
-			
-
-	} 
-
-
-}
-
-void wide_string() {
-
-	next_char(&FileReader); /* skip " */
-	
-	
-
-}
-
-void string() {
-
-
-
-}
-
-void identifier(int c) {
-	
-	if (c == 'L' && next_peek(&FileReader) == '"') {
-	
-		wide_string();
-		return;
-	}
-	
-}
-
-void lexer() {
-
-	int c = next_char(&FileReader);
-	if (c == EOF)return;
-	if (c > 127) {
-			
-		exit_compiler();
-	}
-		
-	switch (c) {
-	
-	case '+':
-				
-	case '-':
-
-	case '%':
-	
-	case '?':
-	
-	case '^':
-	
-	case '"':
-		string();
-		break;
-	case '\r':
-		break;
-		
-	default:
-		if (is_digit(c)) {
-
-			Token.lexeme[0] = c;
-			number(c);
-
-		}
-
-		else if (is_alpha(c)) {
-
-			Token.lexeme[0] = c;
-			identifier(c);
-
-		}
-
-		break;
-	}
-
-
-	FileReader.last_line = FileReader.pos - 1;
-	FileReader.line++;
-	
 }
 
 void exit_compiler() {
 
 
-
-	
-	
-	free(Token.lexeme);
 	exit(1);
 }
+
+void lexer(IncludeStack *top) {
+
+	int c = peek();
+
+	if (c > 127) {
+			
+		exit_compiler();
+	}
+	
+	lexer_loop:	
+	switch (c) {
+
+	case '+':
+		advance();
+		if (peek() == '+') {
+			advance(); // consume
+			Token.type = 123;
+
+		}
+		else {
+			Token.type = 12;
+
+		}
+
+	case '-':
+		if (peek() == '-') {
+			next_char(); // consume
+			Token.type = 123;
+
+		}
+		else if (peek() == '=') {
+			next_char(); // consume
+			Token.type = 123;
+
+		}
+		else {
+			Token.type = 12;
+
+		}
+
+	case '%':
+		if (peek() == '=') {
+			next_char(); // consume
+			Token.type = 123;
+
+		}
+		else {
+			Token.type = 12;
+
+		}
+	
+	case '\n':
+		break;
+
+	case '?':
+
+	case '^':
+		if (peek() == '=') {
+			next_char(); // consume
+			Token.type = 123;
+
+		}
+		else {
+			Token.type = 12;
+
+		}
+
+	case '"':
+		string();
+		break;
+	case '\r':
+		break;
+	default:
+		if (is_digit(c)) {
+			number();
+			
+		}
+
+		else if (is_alpha(c)) {
+			identifier();			
+		}
+		else {
+
+			exit_compiler();
+		}
+		break;
+	}
+		
+	c = peek();
+	
+	if (c == EOF) {
+
+	}
+	
+	goto lexer_loop;
+
+	FileReader.last_line = FileReader.pos;
+	FileReader.line++;
+	
+}
+
 
 
 
@@ -401,19 +307,12 @@ void include_lexeme(IncludeStack** top, int c) {
 void lexer_start(const char * source) {
 	
 	IncludeStack* top = NULL;
-	push_file(&top, source, &FileReader);
+	push_file(&top, source);	
+	lexer(top);		
 		
-	while (!FileReader.eof) {				
-		
-		lexer();		
-		
-	}	
-	
 }
 
 int main(int argc , char *argv[]) {
-	
-	Token.lexeme = malloc(LEXEME_SIZE);
 	
 	lexer_start(argv[1]);
 
