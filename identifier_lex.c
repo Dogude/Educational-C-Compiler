a26626a435f8bde99a39a5b21963c7ad1df02c06
polaris <131747396+Dogude@users.noreply.github.com>
2026-09-06 21:09:35 +0200

    Refactor identifier function and add new string handling

--- identifier_lex.c
+++ identifier_lex.c
@@ -4,27 +4,132 @@ enum IdentifierState {
 
 
 
+
 };
+
+void name() {
+
+	int c = peek();
+	while (is_alpha(c)) {
+
+		if (Token.index >= LEXEME_SIZE - 1) {
+
+			exit_compiler();
+		}
+
+		Token.lexeme[Token.index++] = c;
+		advance();
+		c = peek();	
+	}
+
+}
+
+
+void c16_string() {
+
+
+
+}
+
+void string() {
+
+	
+
+}
+
+
+void char32_tf() {
+
+
+
+}
+
+void c32_string() {
+
+
+
+}
+
+void char16_tf() {
+
+
+
+}
+
+void wide_string() {
+	
+	Token.index = 0;
+
+}
 
 void identifier() {
 
+	int c = peek();
 
-	int state = INTEGER;
-	int index = 0;
+	Token.index = 0;
 
-	do {
+	switch (c) {
 
-		switch (state) {
+	case 'L':
+		Token.lexeme[Token.index++] = c;
+		advance();
+		if (peek() == '"') {
+			advance();
+			wide_string();
+		}
+		else if (peek() == '\'') {
+			
+		}
+		else {
+			name();
+		}
+		break;
+	
+	case 'u':
+		Token.lexeme[Token.index++] = c;
+		advance();
+		if (peek() == '"') {
+			advance();
+			c16_string();
+		}
+		else if (peek() == '\'') {
+			advance();
+			char16_tf();
+		}
+		else if (peek() == '8') {
+			advance();
+			if (peek() == '"') {
+				advance();
 
+			}
+			else if (peek() == '\'') {
+				advance();
+				
+			}
+		}
+		else {
+			name();
 
+		}
+		break;
+	
+	case 'U':
+		Token.lexeme[Token.index++] = c;
+		advance();
+		if (peek() == '"') {
+			advance();
+			c32_string();
+		}
+		else if (peek() == '\'') {
+			advance();
+			char32_tf();
 
-		if (state == SUCCESS)
-			return c;
+		}
+		else {
+			name();
+		}
+		break;
 
-		index++;
-		c = next_char();
-
-	} while (0);
-
-
+	};
+	
 }
