360b09d1ec53eca2059aa19edb80cdaf9cc00b86
polaris <131747396+Dogude@users.noreply.github.com>
2026-09-06 22:27:56 +0200

    Implement keyword checking in identifier_lex.c
    Added a check_keyword function to identify keywords in the token lexeme based on its length. The function handles various keyword cases and sets the token type accordingly.

--- identifier_lex.c
+++ identifier_lex.c
@@ -1,11 +1,111 @@
 #include "lexer.h"
 
-enum IdentifierState {
+void check_keyword() {
 
+	/* may be switched to state of chars later */
 
+	int longest = sizeof("_STATIC_ASSERT") - 1;
+	
+	int len = Token.index - 1;
 
+	if (len > longest)
+		return;
 
-};
+	switch (len) {
+	
+	case 2:
+		if (strcnmp(Token.lexeme, "if", 2) == 0)
+			Token.type = IF;
+		break;
+	
+	case 4:
+		if (strcnmp(Token.lexeme, "else", 4) == 0)
+			Token.type = ELSE;
+		else if (strcnmp(Token.lexeme, "case", 4) == 0)
+			Token.type = CASE;
+		else if (strcnmp(Token.lexeme, "elif", 4) == 0)
+			Token.type = ELIF;
+		else if (strcnmp(Token.lexeme, "line", 4) == 0)
+			Token.type = LINE;
+		else if (strcnmp(Token.lexeme, "true", 4) == 0)
+			Token.type = TRUE;
+		else if (strcnmp(Token.lexeme, "bool", 4) == 0)
+			Token.type = BOOL;
+		else if (strcnmp(Token.lexeme, "auto", 4) == 0)
+			Token.type = AUTO;
+		else if (strcnmp(Token.lexeme, "long", 4) == 0)
+			Token.type = LONG;		
+		break;
+	
+	case 5:
+		if (strcnmp(Token.lexeme, "while", 5) == 0)
+			Token.type = WHILE;
+		else if (strcnmp(Token.lexeme, "embed", 5) == 0)
+			Token.type = EMBED;
+		else if (strcnmp(Token.lexeme, "undef", 5) == 0)
+			Token.type = UNDEF;
+		else if (strcnmp(Token.lexeme, "error", 5) == 0)
+			Token.type = ERROR;
+		else if (strcnmp(Token.lexeme, "endif", 5) == 0)
+			Token.type = ENDIF;
+		else if (strcnmp(Token.lexeme, "false", 5) == 0)
+			Token.type = FALSE;
+		break;
+	
+	case 7:
+		if (strcnmp(Token.lexeme, "elifdef", 7) == 0)
+			Token.type = ELIFDEF;
+		else if (strcnmp(Token.lexeme, "include", 7) == 0)
+			Token.type = INCLUDE;
+		else if (strcnmp(Token.lexeme, "warning", 7) == 0)
+			Token.type = WARNING;
+		else if (strcnmp(Token.lexeme, "defined", 7) == 0)
+			Token.type = DEFINED;
+		else if (strcnmp(Token.lexeme, "alignas", 7) == 0)
+			Token.type = ALIGNAS;
+		else if (strcnmp(Token.lexeme, "alignof", 7) == 0)
+			Token.type = ALIGNOF;
+		else if (strcnmp(Token.lexeme, "typedef", 7) == 0)
+			Token.type = TYPEDEF;
+		else if (strcnmp(Token.lexeme, "nullptr", 7) == 0)
+			Token.type = NULLPTR;
+		break;
+	
+	case 6:
+		if (strcnmp(Token.lexeme, "pragma", 6) == 0)
+			Token.type = PRAGMA;
+		else if (strcnmp(Token.lexeme, "extern", 6) == 0)
+			Token.type = EXTERN;
+		else if (strcnmp(Token.lexeme, "static", 6) == 0)
+			Token.type = STATIC;
+		else if (strcnmp(Token.lexeme, "inline", 6) == 0)
+			Token.type = INLINE;
+		else if (strcnmp(Token.lexeme, "define", 6) == 0)
+			Token.type = DEFINE;
+		break;
+	
+	case 8:
+		if (strcnmp(Token.lexeme, "elifndef", 8) == 0)
+			Token.type = ELIFNDEF;
+		else if (strcnmp(Token.lexeme, "register", 8) == 0)
+			Token.type = REGISTER;
+		else if (strcnmp(Token.lexeme, "restrict", 8) == 0)
+			Token.type = RESTRICT;
+		break;
+	
+	case 9:
+		if (strcnmp(Token.lexeme, "constexpr", 9) == 0)
+			Token.type = CONSTEXPR;
+		break;
+	
+	case 14:
+		if (strcnmp(Token.lexeme, "_STATIC_ASSERT", 14) == 0)
+			Token.type = _STATIC_ASSERT;
+		break;
+	
+	};
+
+}
 
 void name() {
 
