9a550284ad2275d690da3ecd85a2c8c027f033a4
polaris <131747396+Dogude@users.noreply.github.com>
2026-09-08 22:43:31 +0200

    Implement parser functions for token handling

--- parser.c
+++ parser.c
@@ -1,15 +1,142 @@
 #include "parser.h"
 
-extern struct Token Token;
+void start() {
 
-extern void exit_compiler();
+	switch (Token.type) {
 
-extern void print_line();
+	case SHARP:
+		state = PREPROCESSOR;
+		break;
 
+	case TYPEDEF:
+		break;
+
+	case NEW_LINE:
+		break;
+
+	case STATIC:
+		break;
+
+	case INLINE:
+		break;
+		
+	case ENUM:
+	
+		break;
+	
+	case STRUCT:
+		break;
+	
+	case INT:
+		break;
+	case VOID:
+		break;
+	case DOUBLE:
+		break;
+	case FLOAT:
+		BREAK;
+	
+	default:
+		// error
+		break;
+
+	};
+
+}
+
+void preprocessor() {
+
+	switch (Token.type) {
+
+	case DEFINE:
+		break;
+	case ELIFDEF:
+		break;
+	case ELIFNDEF:
+		break;
+	case INCLUDE:
+		break;
+	case EMBED:
+		state = EMBED_DIRECTIVE;
+		break;
+	case IFDEF:
+		break;
+	case IF:
+		break;
+	case ELSE:
+		break;
+	case ELIF:
+		break;
+	
+	};
+
+}
+
+void embed_directive() {
+
+	enum EmbedState {
+
+		SEQUENCE,
+		PARAMETER
+	};
+
+	int embed_state = SEQUENCE;
+
+	embed:
+
+	lexer();
+
+	switch (embed_state) {
+	
+	case SEQUENCE:
+		if (Token.type == 0) {
+			FILE* embed_file = fopen(Token.lexeme,"rb");
+			
+			state = PARAMETER;
+		}
+		break;
+			
+	case PARAMETER:
+		if (Token.type == EMBED_IF_EMPTY) {
+			
+		}
+		break;
+
+	default:
+		break;
+	};
+
+
+	goto embed;
+
+}
 
 void parser() {
 
+	state = START;
 
+	parser_loop:
 
+	lexer();
+
+	switch (state) {
+
+	case START:
+		start();
+		break;
+
+	case PREPROCESSOR:
+		preprocessor();
+		break;
+	
+	case EMBED_DIRECTIVE:
+		embed_directive();
+		break;
+
+	default:
+		break;
+	}
+
+	goto parser_loop;
 
 }
