a740144c5d1c9ae7ce5f34a9b01e4ea043d8bb00
polaris <131747396+Dogude@users.noreply.github.com>
2026-09-18 23:48:48 +0200

    Implement PRAGMA directive and enhance embed handling
    Added support for PRAGMA directives and improved embed directive handling.

--- parser.c
+++ parser.c
@@ -27,6 +27,8 @@ void start() {
 	case STRUCT:
 		break;
 	
+	case CONST:
+		break;
 	case INT:
 		break;
 	case VOID:
@@ -36,6 +38,7 @@ void start() {
 	case FLOAT:
 		BREAK;
 	
+
 	default:
 		// error
 		break;
@@ -67,47 +70,112 @@ void preprocessor() {
 		break;
 	case ELIF:
 		break;
+	case PRAGMA:
+		state = PRAGMA_DIRECTIVE;
+		break;
 	
 	};
 
 }
 
+/* a comma seperated unsigned char array for context */
 void embed_directive() {
 
 	enum EmbedState {
 
 		SEQUENCE,
-		PARAMETER
+		PARAMETER,
+		PARAMETER_OPEN,
+		PARAMETER_CLOSE,
+		FINISH
 	};
 
 	int embed_state = SEQUENCE;
-
-	embed:
-
-	lexer();
-
-	switch (embed_state) {
 	
-	case SEQUENCE:
-		if (Token.type == 0) {
-			FILE* embed_file = fopen(Token.lexeme,"rb");
-			
-			state = PARAMETER;
-		}
-		break;
-			
-	case PARAMETER:
-		if (Token.type == EMBED_IF_EMPTY) {
-			
-		}
-		break;
+	FILE* file;
 
-	default:
-		break;
-	};
+	Token.state = FILE_MOD; // switch to lexer mode to read file path in ""
 
+	while (1) {
 
-	goto embed;
+		lexer();
+
+		switch (embed_state) {
+
+		case SEQUENCE:
+		
+			switch (Token.type) {
+
+			case FILE_STR:
+				embed_state = PARAMETER;
+				Token.state = STR_MOD;
+				break;
+			default:
+				// error case
+				break;
+
+			};
+			break;
+
+		case PARAMETER:
+			switch (Token.type) {
+
+			case EMBED_IF_EMPTY:
+				break;
+
+			case EMBED_LIMIT:
+				break;
+
+			case EMBED_PREFIX:
+				break;
+
+			case EMBED_SUFFIX:
+				break;
+
+			case NEW_LINE:
+				return;
+
+			default:
+				// error case
+				break;
+
+			};
+			break;
+
+		case PARAMETER_OPEN:
+
+			break;
+
+		case PARAMETER_CLOSE:
+			switch (Token.type) {
+			case CLOSE_PAR:
+				embed_state = FINISH;
+				break;
+			default:
+				// error
+				break;
+			};
+			break;
+
+		case FINISH:
+			switch (Token.type) {
+			case NEW_LINE:
+				return;
+			default:
+				// error
+				break;
+			};
+			break;
+
+		};
+
+	}
+
+}
+
+void pragma_directive() {
+
+	
 
 }
 
@@ -115,28 +183,246 @@ void parser() {
 
 	state = START;
 
-	parser_loop:
+	while (1) {
 
-	lexer();
+		lexer();
 
-	switch (state) {
+		switch (state) {
 
-	case START:
-		start();
-		break;
+		case START:
+			start();
+			break;
 
-	case PREPROCESSOR:
-		preprocessor();
-		break;
+		case PREPROCESSOR:
+			preprocessor();
+			break;
+
+		case EMBED_DIRECTIVE:
+			embed_directive();
+			break;
+
+		case PRAGMA_DIRECTIVE:
+			pragma_directive();
+			break;
+
+		default:
+			break;
+		}
 	
-	case EMBED_DIRECTIVE:
-		embed_directive();
-		break;
-
-	default:
-		break;
 	}
 
-	goto parser_loop;
+}
+#include "parser.h"
+
+void start() {
+
+	switch (Token.type) {
+
+	case SHARP:
+		state = PREPROCESSOR;
+		break;
+
+	case TYPEDEF:
+		break;
+
+	case NEW_LINE:
+		break;
+
+	case STATIC:
+		break;
+
+	case INLINE:
+		break;
+		
+	case ENUM:
+	
+		break;
+	
+	case STRUCT:
+		break;
+	
+	case CONST:
+		break;
+	case INT:
+		break;
+	case VOID:
+		break;
+	case DOUBLE:
+		break;
+	case FLOAT:
+		BREAK;
+	
+
+	default:
+		// error
+		break;
+
+	};
+
+}
+
+void preprocessor() {
+
+	switch (Token.type) {
+
+	case DEFINE:
+		break;
+	case ELIFDEF:
+		break;
+	case ELIFNDEF:
+		break;
+	case INCLUDE:
+		break;
+	case EMBED:
+		state = EMBED_DIRECTIVE;
+		break;
+	case IFDEF:
+		break;
+	case IF:
+		break;
+	case ELSE:
+		break;
+	case ELIF:
+		break;
+	case PRAGMA:
+		state = PRAGMA_DIRECTIVE;
+		break;
+	
+	};
+
+}
+
+/* a comma seperated unsigned char array for context */
+void embed_directive() {
+
+	enum EmbedState {
+
+		SEQUENCE,
+		PARAMETER,
+		PARAMETER_OPEN,
+		PARAMETER_CLOSE,
+		FINISH
+	};
+
+	int embed_state = SEQUENCE;
+	
+	FILE* file;
+
+	Token.state = FILE_MOD; // switch to lexer mode to read file path in ""
+
+	while (1) {
+
+		lexer();
+
+		switch (embed_state) {
+
+		case SEQUENCE:
+		
+			switch (Token.type) {
+
+			case FILE_STR:
+				embed_state = PARAMETER;
+				Token.state = STR_MOD;
+				break;
+			default:
+				// error case
+				break;
+
+			};
+			break;
+
+		case PARAMETER:
+			switch (Token.type) {
+
+			case EMBED_IF_EMPTY:
+				break;
+
+			case EMBED_LIMIT:
+				break;
+
+			case EMBED_PREFIX:
+				break;
+
+			case EMBED_SUFFIX:
+				break;
+
+			case NEW_LINE:
+				return;
+
+			default:
+				// error case
+				break;
+
+			};
+			break;
+
+		case PARAMETER_OPEN:
+
+			break;
+
+		case PARAMETER_CLOSE:
+			switch (Token.type) {
+			case CLOSE_PAR:
+				embed_state = FINISH;
+				break;
+			default:
+				// error
+				break;
+			};
+			break;
+
+		case FINISH:
+			switch (Token.type) {
+			case NEW_LINE:
+				return;
+			default:
+				// error
+				break;
+			};
+			break;
+
+		};
+
+	}
+
+}
+
+void pragma_directive() {
+
+	
+
+}
+
+void parser() {
+
+	state = START;
+
+	while (1) {
+
+		lexer();
+
+		switch (state) {
+
+		case START:
+			start();
+			break;
+
+		case PREPROCESSOR:
+			preprocessor();
+			break;
+
+		case EMBED_DIRECTIVE:
+			embed_directive();
+			break;
+
+		case PRAGMA_DIRECTIVE:
+			pragma_directive();
+			break;
+
+		default:
+			break;
+		}
+	
+	}
 
 }
