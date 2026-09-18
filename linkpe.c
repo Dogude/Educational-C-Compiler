4b32b081a89b976241c59fa9b7fa3f599bd9a299
polaris <131747396+Dogude@users.noreply.github.com>
2026-09-18 21:44:15 +0200

    Refactor linkpe.c to define PE sections and data structures

--- linkpe.c
+++ linkpe.c
@@ -1,17 +1,125 @@
-#include <stdio.h>
+#include "lexer.h"
 
-// text segment
-typedef struct {
-    unsigned char* data;
-    size_t size;
-    size_t capacity;
-} x64_Opcodes;
+#define FILE_ALIGN 0x200
 
-// data segment
-typedef struct {
+struct section_bss {
+    // name 8 bytes , ".bss\0\0\0\0"
+    unsigned int VirtualSize;
+    unsigned int VirtualAddress;
+    unsigned int SizeOfRawData;
+    unsigned int PointerToRawData;
+    unsigned int PointerToRelocations;
+    unsigned int PointerToLinenumbers;
+    unsigned short NumberOfRelocations;
+    unsigned short NumberOfLinenumbers;
+    unsigned int Characteristics; // 0xC0000080
+} section_bss;
+
+struct section_text {
+    // name 8 bytes , ".text\0\0\0\0"
+    unsigned int VirtualSize;
+    unsigned int VirtualAddress;
+    unsigned int SizeOfRawData;
+    unsigned int PointerToRawData;
+    unsigned int PointerToRelocations;
+    unsigned int PointerToLinenumbers;
+    unsigned short NumberOfRelocations;
+    unsigned short NumberOfLinenumbers;
+    unsigned int Characteristics; // 
+} section_text;
+
+// section
+struct section_data {
+    // name 8 bytes , ".data\0\0\0"
+    unsigned int VirtualSize;
+    unsigned int VirtualAddress;
+    unsigned int SizeOfRawData;
+    unsigned int PointerToRawData;
+    unsigned int PointerToRelocations;
+    unsigned int PointerToLinenumbers;
+    unsigned short NumberOfRelocations;
+    unsigned short NumberOfLinenumbers;
+    unsigned int Characteristics; // 
+} section_data;
+
+struct section_edata {
+    // name 8 bytes , ".edata\0\0\0"
+    unsigned int VirtualSize;
+    unsigned int VirtualAddress;
+    unsigned int SizeOfRawData;
+    unsigned int PointerToRawData;
+    unsigned int PointerToRelocations;
+    unsigned int PointerToLinenumbers;
+    unsigned short NumberOfRelocations;
+    unsigned short NumberOfLinenumbers;
+    unsigned int Characteristics; // 
+} section_edata;
+
+// cap for SizeOfRawData (alloc multiple of 0x200)
+// len for VirtualSize
+
+// text segment read exec
+struct TEXT {   
     unsigned char* data;
-    size_t size;
-    size_t capacity;
-} data;
+    int len;
+    int cap;
+} TEXT;
+
+// data segment read write
+struct DATA {  
+    unsigned char* data;
+    int len;
+    int cap;
+} DATA;
+
+// edata segment , read
+struct eDATA {
+    unsigned char* data;
+    int len;
+    int cap;
+} eDATA;
+
+
+void alloc_pe() {
+
+    DATA.data = malloc(FILE_ALIGN);
+    if (!DATA.data) {
+
+
+        exit_compiler();
+    }
+
+    DATA.cap = FILE_ALIGN;
+    
+}
+
+void write_to_data(int val) {
+
+    if (DATA.len < DATA.cap) {
+
+        DATA.data[DATA.len++] = val;
+
+    }
+    else {
+
+        int new_cap = DATA.cap + FILE_ALIGN;
+
+        void* temp = realloc(DATA.data, new_cap);
+        if (!temp) {
+
+
+            exit_compiler();
+        }
+
+        DATA.data = temp;
+        DATA.cap = new_cap;
+        DATA.data[DATA.len++] = val;
+
+    }
+
+
+}
+
+
 
 
